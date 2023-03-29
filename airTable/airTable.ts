7103c7719863e8b4b6bcbb34915388d87a9f249a
quesnel alexis <quesnelalexis@macbook-air-de-quesnel.home>
2023-03-29 16:12:28 +0200

    fix

--- airTable/airTable.ts
+++ airTable/airTable.ts
@@ -3,7 +3,7 @@ const Airtable = require('airtable');
 export const connectAT = () => {
     Airtable.configure({
         endpointUrl: 'https://api.airtable.com',
-        apiKey: 'keyBa8cBHbsYQDnBE'
+        apiKey: process.env.API_KEY
     });
     return Airtable.base('app4nKak1PRudoiMb');
 }
